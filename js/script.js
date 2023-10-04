03f9dcbecf5cbb5fd4af1ec0117e734a524b8cb3
Pirisinu <palazzialessandro2@gmail.com>
2023-10-04 10:46:55 +0200

    [ADD]html comments

--- js/script.js
+++ js/script.js
@@ -14,18 +14,17 @@ createApp({
   methods:{
     //Gestione del messaggio utente
     messageSend(){
-      const dateCurrent = new Date();
-      const dateFormatted = `
-          ${dateCurrent.getDate()}/${dateCurrent.getMonth() + 1}/${dateCurrent.getFullYear()} ${dateCurrent.getHours()}:${dateCurrent.getMinutes()}:${dateCurrent.getSeconds()}`
+      const dateFormatted = this.getCurrentFormattedDate();
       const messageFormatted = {
         date : dateFormatted,
         message: this.newMessage,
         status: 'sent'
       };
       //Push nella chat
-      this.chatSel.messages.push(messageFormatted);
+      this.pushMessage(messageFormatted)
       //Reset
-      this.newMessage = '';
+      this.resetNewMessageInput();
+
       //timeout di risposta automatica
       setTimeout(()=> {
         const replyMessagge = {
@@ -33,16 +32,29 @@ createApp({
           message: 'ok',
           status: 'received'
         };
-        this.chatSel.messages.push(replyMessagge);
+        this.pushMessage(replyMessagge);
       },1000)
+    },
+    getCurrentFormattedDate(){
+      const dateCurrent = new Date();
+      return `${dateCurrent.getDate()}/${dateCurrent.getMonth() + 1}/${dateCurrent.getFullYear()} ${dateCurrent.getHours()}:${dateCurrent.getMinutes()}:${dateCurrent.getSeconds()}`
+    },
+    pushMessage(message) {
+      if(this.chatSel){
+        this.chatSel.messages.push(message)
+      }
+    },
+    resetNewMessageInput(){
+      this.newMessage = '';
     },
   },
   computed:{
     serchedChat(){
       const charToSearch = this.chatWanted.toLowerCase();
-      return this.contacts.filter(contact => contact.name.toLowerCase().includes(charToSearch))
-    }
-
+      return this.contacts.filter(contact =>
+         contact.name.toLowerCase().includes(charToSearch)
+      );
+    },
   },
   mounted(){
   }
